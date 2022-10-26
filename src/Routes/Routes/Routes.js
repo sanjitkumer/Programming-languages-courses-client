65220f7aee41efa167a8ab6e139bfda6db45daea
sanjitkumer <sanjitk666@gmail.com>
2022-10-26 15:05:52 +0600

    added header part

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Courses from "../../Pages/Courses/Courses";
+import Details from "../../Pages/Details/Details";
+import Home from "../../Pages/Home/Home/Home";
 
 export const routes = createBrowserRouter([
-    
+    {
+        path: '/',
+        element:<Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses/:id',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/details/:id',
+                element: <Details></Details>
+            }
+        ]
+    }
 ])
